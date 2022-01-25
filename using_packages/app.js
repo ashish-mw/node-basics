b1075680d3ede2c2265ada9650a97735616d3bf0
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2022-01-25 12:48:41 +0530

    moment example add

--- using_packages/app.js
+++ using_packages/app.js
@@ -2,7 +2,8 @@ const moment = require("moment");
 
 const name = "Hello";
 const date = moment().format("DD/MM/YYYY hh:mm a");
+const future = moment().add(3, "days");
 
-console.log(date);
+console.log(future.format("DD/MM/YYYY"));
 
 // 25/01/2022 12:45 PM
